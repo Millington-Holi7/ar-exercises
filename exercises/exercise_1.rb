7c723f9e418775e5bddfe2c4da349d4467afa064
Millington-Holi7 <holinas007@gmail.com>
2024-07-03 20:50:03 -0400

    Active record exercise

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,22 @@
 require_relative '../setup'
+=begin
+- Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+- Richmond (annual_revenue of 1260000 carries women's apparel only)
+- Gastown (annual_revenue of 190000 carries men's apparel only)
+  t.column :name, :string
+    t.column :annual_revenue, :integer
+    t.column :mens_apparel, :boolean
+    t.column :womens_apparel, :boolean
+    t.timestamps null: false
+=end
 
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
+store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true )
+store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false )
+
+
+total_stores = Store.count
+puts total_stores
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,8 +1,18 @@
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
+=begin
+1. Load the third store (into `@store3`) as you did the other two before.
+2. Using Active Record's `destroy` method, delete the store from the database.
+3. Verify that the store has been deleted by again outputting (`puts`ing) the `count` (as you did in Exercise 1.)
+=end
 
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+store = Store.create(name: "Apple", annual_revenue: 7700000, mens_apparel: true, womens_apparel: true )
+store = Store.find_by(name: "Apple")
+store.destroy
+
+total_stores = Store.count
+puts total_stores
